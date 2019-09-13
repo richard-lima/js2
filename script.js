731dbf73c9ef61cb4acaec272af8cf0f1570251b
Richard <richardlimaxx@gmail.com>
2019-09-13 11:51:40 -0300

    Terminar site e consertar animacao do menu

--- script.js
+++ script.js
@@ -1,9 +1,28 @@
 let meuHamburguer = document.querySelector(".menu-hamburguer");
 let nav = document.querySelector("nav");
+let input = document.querySelector("input");
+let botao = document.querySelector("button");
+let h2 = document.querySelector("h2");
 
 function mostrarMenu(){
     //nav.style.display = "block";
-    nav.classList.add("visivel");
+//JEITO 1
+//     if(nav.classList.contains("visivel")){
+//         nav.classList.remove("visivel");
+//     }else{
+//         nav.classList.add("visivel");
+//     }
+// 
+//JEITO 2
+    nav.classList.toggle("visivel");
+    menu-Hamburguer.classList.toggle("ativo");
 }
 
+function pegarNome(){
+    let nome = input.value
+    h2.innerHTML = `Seja bem-vind@ ${nome}`;
+    input.value = ""; 
+}
 meuHamburguer.onclick = mostrarMenu;
+
+botao.onclick = pegarNome;
